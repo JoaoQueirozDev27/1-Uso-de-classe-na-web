75adcda996632bc35aa2ea08c981595538f6b40f
JoaoQueirozDev27 <148493641+JoaoQueirozDev27@users.noreply.github.com>
2023-11-14 10:51:05 -0300

    Genero e gerenciadorGenero criados

---
+++ 37279_mostrarjogos/37279/37279/Classes/Genero.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _37279.Classes
+{
+    public class Genero
+    {
+        public string Nome { get; set; }
+    }
+}
---
+++ 37279_mostrarjogos/37279/37279/Classes/GerenciadorGeneros.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _37279.Classes
+{
+    public class GerenciadorGeneros
+    {
+    }
+}
